0599b758287334652111cf7c63a1c96ac5a90ee7
ev-moon <xenos96111@gmail.com>
2022-08-19 10:24:16 +0900

    정렬된 리스트 안에서 범위에 해당하는 숫자 개수 도출

---
+++ count_by_range.py
@@ -0,0 +1,7 @@
+from bisect import bisect_left, bisect_right
+
+# count number of values in sorted list O(N)
+def count_by_range(a, left_value, right_value):
+    right_index = bisect_right(a, right_value)
+    left_index = bisect_left(a, left_value)
+    return right_index - left_index
